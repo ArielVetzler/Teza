2ddf79d239c02cc55834cd4ac134e5665b2f1f9f
Ariel Vetzler <Arielvetzler1@gmail.com>
2025-07-26 18:29:28 +0300

    Clean up committed JS and dist from Git

--- Frontend/src/components/Card/Card.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import './Card.css';
-var Card = function (_a) {
-    var children = _a.children;
-    return (_jsx("div", { className: "card", children: children }));
-};
-export default Card;
--- Frontend/src/components/Controls/Controls.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import './Controls.css';
-var Controls = function (_a) {
-    var onToggleGeneration = _a.onToggleGeneration, onReset = _a.onReset, isRunning = _a.isRunning;
-    return (_jsxs("div", { className: "controls-container", children: [_jsx("button", { onClick: onToggleGeneration, className: "control-button toggle-button", children: isRunning ? 'Pause' : 'Play' }), _jsx("button", { onClick: onReset, className: "control-button reset-button", children: "Reset" })] }));
-};
-export default Controls;
