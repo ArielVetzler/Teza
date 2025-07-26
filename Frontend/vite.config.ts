c0bd78a0f51b8dd20260069a752d2273d4abc4ac
Ariel Vetzler <Arielvetzler1@gmail.com>
2025-07-26 19:13:04 +0300

    test10

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '/', // this is default, but make sure it wasn't changed
+    outDir: 'dist', // this is default, but make sure it wasn't changed
   },
 });
