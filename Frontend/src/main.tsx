c597a38b97ca1658be197173412aff8c5e35f3e5
Ariel Vetzler <Arielvetzler1@gmail.com>
2025-07-26 18:36:47 +0300

    fix env

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App'
+import dotenv from 'dotenv'
+dotenv.config()
 
 
 createRoot(document.getElementById('root')!).render(
